62ff9b6101e594469105f2c1dbcf51b234a4ac33
James Hotblack <james.c.hotblack@gmail.com>
2020-02-28 16:43:33 +0000

    Set up a post route that accepts a cart string, parses it, and generates the list products on the cart, along with their quantities.

--- api/server.js
+++ api/server.js
@@ -17,7 +17,20 @@ app.get('/api/products', (req, res) => { //lists all  available products
   return res.json(data.products);
 });
 
-
+//POST request
+app.post('/api/products', (req, res) => { //generates the list of products in the cart
+  let products = [], id = null;
+  let cart = JSON.parse(req.body.cart);
+  if (!cart) return res.json(products)
+  for (var i = 0; i < data.products.length; i++) {
+    id = data.products[i].id.toString();
+    if (cart.hasOwnProperty(id)) {
+      data.products[i].qty = cart[id]
+      products.push(data.products[i]);
+    }
+  }
+  return res.json(products);
+});
 
 
 
