3332c892dcb1585b2631501d757f1b4cf46b9700
James Hotblack <james.c.hotblack@gmail.com>
2020-02-28 16:22:05 +0000

    api folder contains Server.js file to hold the express backend.

---
+++ api/server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const data = require('./data');
+const middleware = require('./middleware');
