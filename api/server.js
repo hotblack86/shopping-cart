608c783db06265d411f7550a80940650caac5025
James Hotblack <james.c.hotblack@gmail.com>
2020-02-28 18:09:24 +0000

    Set up a login route that authenticates the user, i.e. verifies the user is valid (registered). It generates a JSON web token that expires in two hours. The generated token is used to bypass the login middleware.

--- api/server.js
+++ api/server.js
@@ -32,6 +32,24 @@ app.post('/api/products', (req, res) => { //generates the list of products in th
   return res.json(products);
 });
 
+//Login Route
+app.post('/api/auth', (req,res) => { //signs in user
+  let user = data.users.filter((user) => {
+    return user.name == req.body.name && user.password == req.body.password;
+  });
+  if (user.length){
+    // create a token using user name and password vaild for 2 hours
+    let token_payload = {name: user[0].name, password: user[0].password};
+    let token = jwt.sign(token_payload, "jwt_secret_password", { expiresIn: '2h' });
+    let response = { message: 'Token Created, Authentication Successful!', 
+                     token: token };
+      // return the information including token as JSON
+      return res.status(200).json(response);
+  } else {
+      return res.status("401").json("Authentication failed. admin not found.");
+  }
+});
+
 
 
 
