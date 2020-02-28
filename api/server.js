8dbe85c0e933f8b88d5a99278fdd650fdb1a7a68
James Hotblack <james.c.hotblack@gmail.com>
2020-02-28 16:39:25 +0000

    Set up GET request route in server.js to retreive all products from data.js. Also set up app PORT.

--- api/server.js
+++ api/server.js
@@ -11,3 +11,18 @@ const middleware = require('./middleware');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+// GET request
+app.get('/api/products', (req, res) => { //lists all  available products
+  return res.json(data.products);
+});
+
+
+
+
+
+
+const PORT = 5000;
+
+app.listen(PORT);
+console.log('api runnging on port ' + PORT + ': ');
