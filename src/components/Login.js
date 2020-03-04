0aa64b64931d76acd7bb2900d3fffd63a6cdd70b
James Hotblack <james.c.hotblack@gmail.com>
2020-03-04 17:59:38 +0000

    Added handleInputChange and submitLogin functions to Login React Class. 1st function sets class state. 2nd function calls upon the login function from repository.js to pass username and password onto the backend server and redirects to the home page upon successful completion.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,20 @@
 import React from 'react';
 import { login } from '../repository';
 
+export default class Login extends React.Component{
+  constructor() {
+    super();
+    this.state = { name: '', password: '' };
+  }
 
+  handleInputChange = (event) => this.setState({[event.target.name]: event.target.value})
+      
+  submitLogin = (event) => {
+    event.preventDefault();
+    login(this.state)
+      .then(token => window.location = '/')
+      .catch(err => console.log(err));
+  }
+
+
+}
