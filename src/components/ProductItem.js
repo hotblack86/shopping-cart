19ed07c8b49e9dca2444826cb1b218e2f2c83f1a
James Hotblack <james.c.hotblack@gmail.com>
2020-03-10 17:02:18 +0000

    The addToCart method adds the given product to the cart which is an object stored in localStorage as a string using the JSON.stringify method. This method converts the string back to object using the JSON.parse method or creates a new object if no item is found. The product is then added, and the cart is saved back in localStorage.

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -10,3 +10,17 @@ export default class ProductItem extends React.Component {
   handleInputChange = e => 
     this.setState({[e.target.name]: e.target.value})
   }
+
+  addToCart = () => {
+    let cart = localStorage.getItem('cart') 
+                  ? JSON.parse(localStorage.getItem('cart')) : {};
+    let id = this.props.product.id.toString();
+    cart[id] = (cart[id] ? cart[id]: 0);
+    let qty = cart[id] + parseInt(this.state.quantity);
+    if (this.props.product.available_quantity < qty) {
+      cart[id] = this.props.product.available_quantity; 
+    } else {
+      cart[id] = qty
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
