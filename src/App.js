1a1041c30b960700c8bf7414454392af623a7a9b
James Hotblack <james.c.hotblack@gmail.com>
2020-03-11 17:28:05 +0000

    logOut function in App.js that removes the access token.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Checkout from './components/Checkout';
 import {  BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import { isAuthenticated } from './repository';
 
+class App extends Component {
+  
+  logOut(){
+    localStorage.removeItem('x-access-token');
+  }
+
+
+}
+
 
 
 export default App;
