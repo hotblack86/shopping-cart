b43dc7879f44527bf818d4dc01e3e36443d3f874
James Hotblack <james.c.hotblack@gmail.com>
2020-03-11 17:26:13 +0000

    Editing App.js to import necessesary files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
+import Login from './components/Login';
+import Products from './components/ProductList';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import {  BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { isAuthenticated } from './repository';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
 export default App;
